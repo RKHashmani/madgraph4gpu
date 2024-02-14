7527c9334f2b1e59c387671d0105ae6e1c3dee85
Raheem Hashmani <RKHashmani@gmail.com>
2024-02-14 11:56:00 -0600

    Testing File Creation

--- epochX/cudacpp/gg_ttgg.sa/SubProcesses/profile.sh
+++ epochX/cudacpp/gg_ttgg.sa/SubProcesses/profile.sh
@@ -181,8 +181,10 @@ else
   echo "PROFILING: ${cmd}"
   echo "${ncu} --metrics ${metrics} ${cmd}"
   echo
-  echo sudo LD_LIBRARY_PATH=${LD_LIBRARY_PATH} $(which ${ncu}) --metrics ${metrics}  --target-processes all ${cmd}
+  echo sudo LD_LIBRARY_PATH=${LD_LIBRARY_PATH} $(which ${ncu}) --metrics ${metrics}  --target-processes all ${cmd} -o results_output
   echo DEBUG 1
   sudo LD_LIBRARY_PATH=${LD_LIBRARY_PATH} $(which ${ncu}) --metrics ${metrics}  --target-processes all ${cmd} -o results_output
+  echo sudo touch test.txt
+  sudo touch test.txt
 
 fi
